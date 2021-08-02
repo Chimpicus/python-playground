cba19cc56f74eb2ffb69c176fe95ddcb5c8285de
Chimpicus <caseyboyes@gmail.com>
2021-08-02 15:47:54 +0100

    add savingsCalc

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,18 +1,17 @@
 def fizzBuzz():
     for num in range(1, 100):
-        if num%3 == 0 and num%5 == 0:
+        if num % 3 == 0 and num % 5 == 0:
             print('fizzbuzz')
             continue
-        elif num%3 == 0:
+        elif num % 3 == 0:
             print('fizz')
             continue
-        elif num%5 ==0:
+        elif num % 5 == 0:
             print('buzz')
             continue
-        elif num%3 != 0 and num%5 != 0:
+        elif num % 3 != 0 and num % 5 != 0:
             print(num)
-            
-        
-#the indentation is extremely important in python.
+
+
+# the indentation is extremely important in python.
 fizzBuzz()
-        
---
+++ savingsCalculator.py
@@ -0,0 +1,13 @@
+print("Savings Calculator!")
+
+user_wage = input("how much do you make per hour? ")
+user_hours = input("how many hours do you work per day? ")
+user_days = input("how many days do you work per month? ")
+user_earnings = round(
+    (float(user_wage) * float(user_hours)) * int(user_days), 2)
+user_savings = user_earnings / 5
+output = round(user_savings, 2)
+
+
+print("your monthly earnings are: £", user_earnings)
+print("your monthly savings should be: £", output)
